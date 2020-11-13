a831b331ff66ea6d181edaaf09635cf4c8c9a310
Jerome Gratelle <jerome.gratelle@air-indemnite.com>
2020-11-13 13:46:06 +0100

    seed generate doses

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,6 @@ puts 'Creating ingredients..."-'
 
 Ingredient.create(name: "Ice")
 Ingredient.create(name: "Mint leaves")
-
 url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
 ingredients_serialized = open(url).read
 ingredients = JSON.parse(ingredients_serialized)['drinks']
@@ -34,11 +33,22 @@ puts 'Creating cocktails..."-'
 
 50.times { Cocktail.create(name: Faker::ProgrammingLanguage.unique.name) }
 
-url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
-ingredients_serialized = open(url).read
-ingredients = JSON.parse(ingredients_serialized)['drinks']
-ingredients.each do |ingredient|
-  Ingredient.create(name: ingredient['strIngredient1'])
+puts 'Doses seeding'
+puts 'Cleaning database...'
+Dose.destroy_all
+
+puts 'Creating doses..."-'
+cocktails = Cocktail.all
+ingredients = Ingredient.all
+cocktails.each do |cocktail|
+  5.times {
+    description = rand(1..5)
+    dose = Dose.new(description: description)
+    dose.cocktail = cocktail
+    ingredient = ingredients[rand(1..ingredients.length)]
+    dose.ingredient = ingredient
+    dose.save
+  }
 end
 
 puts 'Finished!'
